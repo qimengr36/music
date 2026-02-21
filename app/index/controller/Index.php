ff973597a71164f417dad75d8ea6864ae5faa675
qimengyun <qimengyun2020@163.com>
2026-02-21 17:04:48 +0800

    更新

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -9,11 +9,16 @@ class Index
 {
     public function index()
     {
-        $domain = Request::domain();
-        $env = env('APP_DEBUG', true);
         // 赋值给模板
+        $domain = Request::domain();
         View::assign('domain', $domain);
+        $env = env('APP_DEBUG', true);
         View::assign('env', $env);
+        $link_two = [
+            'name' => '宝藏',
+            'link' => 'https://36haxb.onelink.me/MoVT/82y2yrp9',
+        ];
+        View::assign('link_two', $link_two);
         return View::fetch();
     }
 }
